4920fc7deed30c3e082aacaed139bbcea7d01a88
John Rey Rabosa <johnreyrabosa19@gmail.com>
2024-12-01 14:54:58 +0800

    Add README and update resource paths for Main class

---
+++ README.md
@@ -0,0 +1,8 @@
+# Secret Calculator
+
+## Overview
+Secret Calculator is a Java-based calculator application with a hidden feature. When a certain equation is typed, a new window pops up or something is hidden.
+
+
+## License
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details
--- src/main/java/com/r4ppz/Main.java
+++ src/main/java/com/r4ppz/Main.java
@@ -17,15 +17,15 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/com/r4ppz/secret_calculator/fxml/MainView.fxml")));
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/fxml/MainView.fxml")));
         Scene scene = new Scene(root);
         primaryStage.setScene(scene);
         primaryStage.setResizable(false);
         primaryStage.setTitle("Calculator");
 
-        scene.getStylesheets().add(Objects.requireNonNull(getClass().getResource("/com/r4ppz/secret_calculator/css/ModernMinimalistStyle.css")).toExternalForm());
+        scene.getStylesheets().add(Objects.requireNonNull(getClass().getResource("/css/ModernMinimalistStyle.css")).toExternalForm());
 
-        Image imageIcon = new Image(Objects.requireNonNull(getClass().getResourceAsStream("/com/r4ppz/secret_calculator/image/mainIcon.png")));
+        Image imageIcon = new Image(Objects.requireNonNull(getClass().getResourceAsStream("/image/mainIcon.png")));
         primaryStage.getIcons().add(imageIcon);
 
         primaryStage.show();
